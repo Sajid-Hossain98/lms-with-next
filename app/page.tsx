31c32aab061d3a9244e3cee83c3be473270db8fc
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-09-24 15:41:26 +0600

    authentication using clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      <p className="text-3xl font-medium text-sky-700 flex items-center justify-center h-screen">
-        Hello there!
-      </p>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
