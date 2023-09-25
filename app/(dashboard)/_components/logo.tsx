1440d29409b62fc510250a4876fbee3c18bbec2e
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-09-25 22:09:23 +0600

    prisma schema for course creation

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Logo = () => {
-  return <Image height={100} width={80} alt="logo" src="/logo.svg" />;
+  return (
+    <Link href="/" className="min-w-fit">
+      <Image height={100} width={80} alt="logo" src="/logo.svg" />
+    </Link>
+  );
 };
 
 export default Logo;
