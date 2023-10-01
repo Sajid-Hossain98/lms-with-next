0c2c5934ba9b416f36ed159adbd6929b33053741
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-10-01 21:13:42 +0600

    Loader while reordering is being done

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -12,7 +12,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
-import { Pencil, PlusCircle } from "lucide-react";
+import { Loader2, PlusCircle } from "lucide-react";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
@@ -75,7 +75,13 @@ const ChaptersForm = ({ initialData, courseId }: ChaptersFormProps) => {
     }
   };
   return (
-    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+    <div className="relative mt-6 border bg-slate-100 rounded-md p-4">
+      {isUpdating && (
+        <div className="absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-md flex items-center justify-center">
+          <Loader2 className="animate-spin h-12 w-12 text-sky-700" />
+        </div>
+      )}
+
       <div className="font-medium flex items-center justify-between">
         Course Chapters
         <Button onClick={toggleCreating} variant="ghost">
