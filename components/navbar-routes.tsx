2922d4f05732f3024932f5d7bb8c5971eba4ac22
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-10-07 20:33:58 +0600

    Course search component with useDebounce hook

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -5,40 +5,49 @@ import { usePathname } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { GraduationCap, LogOut } from "lucide-react";
 import Link from "next/link";
+import SearchInput from "./search-input";
 
 const NavbarRoutes = () => {
   const pathname = usePathname();
 
   const isTeacherPage = pathname?.startsWith("/teacher");
   const isPlayerPage = pathname?.includes("/chapter");
+  const isSearchPage = pathname === "/search";
 
   return (
-    <div className="flex gap-x-2 ml-auto">
-      {isTeacherPage || isPlayerPage ? (
-        <Link href="/">
-          <Button
-            size="sm"
-            variant="secondary"
-            className="hover:bg-slate-300/40"
-          >
-            <LogOut className="h-4 w-4 mr-2" />
-            Exit
-          </Button>
-        </Link>
-      ) : (
-        <Link href="/teacher/courses">
-          <Button
-            size="sm"
-            variant="secondary"
-            className="hover:bg-slate-300/40"
-          >
-            <GraduationCap className="h-4 w-4 mr-2" />
-            Teacher Mode
-          </Button>
-        </Link>
+    <>
+      {isSearchPage && (
+        <div className="hidden md:block">
+          <SearchInput />
+        </div>
       )}
-      <UserButton afterSignOutUrl="/" />
-    </div>
+      <div className="flex gap-x-2 ml-auto">
+        {isTeacherPage || isPlayerPage ? (
+          <Link href="/">
+            <Button
+              size="sm"
+              variant="secondary"
+              className="hover:bg-slate-300/40"
+            >
+              <LogOut className="h-4 w-4 mr-2" />
+              Exit
+            </Button>
+          </Link>
+        ) : (
+          <Link href="/teacher/courses">
+            <Button
+              size="sm"
+              variant="secondary"
+              className="hover:bg-slate-300/40"
+            >
+              <GraduationCap className="h-4 w-4 mr-2" />
+              Teacher Mode
+            </Button>
+          </Link>
+        )}
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </>
   );
 };
 
