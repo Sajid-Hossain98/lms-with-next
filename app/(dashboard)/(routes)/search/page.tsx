d5982c145996e88a845cb55f0e7ebde79993f8f0
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-10-08 22:33:16 +0600

    course-card component

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -36,7 +36,7 @@ const SearchPage = async ({ searchParams }: SearchPageProps) => {
       <div className="px-6 pt-6 md:mb-0 md:hidden block">
         <SearchInput />
       </div>
-      <div className="p-6">
+      <div className="p-6 space-y-4">
         <Categories items={categories} />
         <CoursesList items={courses} />
       </div>
