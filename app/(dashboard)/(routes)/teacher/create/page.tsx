24d2f54037a40d6a32ba2357704a15e41399ed56
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-10-14 22:00:43 +0600

    adjusting the href of cancel in the course creation page

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -82,7 +82,7 @@ const CreatePage = () => {
             />
 
             <div className="flex items-center gap-x-2">
-              <Link href="/">
+              <Link href="/teacher/courses">
                 <Button variant="ghost" type="button">
                   Cancel
                 </Button>
